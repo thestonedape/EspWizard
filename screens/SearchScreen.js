775d1bfd2dff36258e5d3e6dd77a628d5d55c530
Its-Nishant-01 <n1shanntjha@gmail.com>
2023-05-03 18:42:12 +0530

    18.43.03.05

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,4 +1,11 @@
-import { useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
+import { Text, TouchableOpacity, View, FlatList, StatusBar, StyleSheet, ToastAndroid } from 'react-native';
+import dgram from 'react-native-udp';
+import Lottie from 'lottie-react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import { useNavigation } from '@react-navigation/native';
+import { useDispatch, useSelector } from 'react-redux';
+
 
 export default function NewDevice() {
     const [loading, setLoading] = useState(false);
@@ -8,38 +15,48 @@ export default function NewDevice() {
     const navigation = useNavigation();
     const dispatch = useDispatch();
 
-    const beginSearch = useCallback(async (broadcastIP) => {
+    const broadcastIP = useSelector((state) => state.IPaddress);
+
+    const beginSearch = useCallback(async () => {
         setLoading(true);
         setFoundEspList([]);
         setDevicesFound(false);
         const port = 12345;
-        socketRef.current.on('message', handleMessage);
-        await socketRef.current.bind(port);
-        socketRef.current.send('ESP-ACK', undefined, undefined, port, broadcastIP, (err) => {
+        const socket = dgram.createSocket('udp4');
+        socket.on('message', handleMessage);
+        await socket.bind(port);
+        socket.send('ESP-ACK', undefined, undefined, port, broadcastIP, (err) => {
             if (err) throw err;
         });
+        socketRef.current = socket;
     }, []);
+    
 
-    function handleMessage(msg, rinfo) {
+    const handleMessage = useCallback((msg, rinfo) => {
         const buffer = msg.toString();
         if (buffer !== 'ESP-ACK' && buffer !== '') {
             const esp = `ESP ${foundEspList.length + 1}`;
             const existingEspIndex = foundEspList.findIndex((item) => item.host === rinfo.address);
+            const updatedEspList = [...foundEspList];
             if (existingEspIndex !== -1) {
-                const updatedEspList = [...foundEspList];
                 updatedEspList[existingEspIndex] = {
+                    ...updatedEspList[existingEspIndex],
+                    key: esp,
+                    message: buffer,
+                };
+            } else {
+                updatedEspList.push({
                     key: esp,
                     host: rinfo.address,
                     message: buffer,
-                };
-                setFoundEspList(updatedEspList);
-            } else {
-                setFoundEspList((prevList) => [...prevList, { key: esp, host: rinfo.address, message: buffer, },]);
+                });
             }
+            setFoundEspList(updatedEspList);
             setDevicesFound(true);
             console.log('Message received and processed successfully:', buffer);
         }
-    }
+    }, [foundEspList]);
+    
 
     const cancelSearch = useCallback(() => {
         setLoading(false);
@@ -51,23 +68,29 @@ export default function NewDevice() {
 
     useEffect(() => {
         dispatch({ type: 'SET_SOCKET', payload: socketRef.current });
+        socketRef.current.on('message', handleMessage);
         return () => {
-            socketRef.current.removeListener('message', handleMessage);
+            socketRef.current.off('message', handleMessage);
         };
     }, [dispatch, handleMessage]);
 
     useEffect(() => {
-        beginSearch(useSelector((state) => state.IPaddress));
+        if (devicesFound && foundEspList.length === 1) {
+            console.log('Found an ESP8266 on the network!', foundEspList[foundEspList.length - 1]?.host);
+        }
+    }, [devicesFound, foundEspList]);
+
+    const handleDeviceSelect = useCallback((item) => {
+        navigation.navigate('Control', { host: item.host });
+    }, [navigation]);
+
+    useEffect(() => {
+        beginSearch();
     }, [beginSearch]);
-
-    function handleDeviceSelect(item) {
-        navigation.navigate('Control', { esp: item });
-    }
-
 
     return (
         <View style={styles.container}>
-            <StatusBar backgroundColor="#000000" barStyle="light-content" />
+          <StatusBar backgroundColor="#000000" barStyle="light-content" />
             <View style={styles.lottieContainer}>
                 <Lottie
                     style={styles.lottie}
@@ -76,18 +99,17 @@ export default function NewDevice() {
                     loop
                 />
                 {foundEspList.length > 0 && (
-                    <View
-                        style={{
-                            padding: 10,
-                            position: 'absolute',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            width: 300,
-                        }}>
-                        <FlatList
-                            data={foundEspList}
-                            renderItem={({ item }) => (
-                                <TouchableOpacity
+                    <View 
+                    style= {{
+                        padding: 10,
+                        position: 'absolute',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        width: 300,}}>
+                    <FlatList
+                                data={foundEspList}
+                                renderItem={({ item }) => (
+                                    <TouchableOpacity 
                                     style={{
                                         alignSelf: 'center',
                                     }}
@@ -95,30 +117,30 @@ export default function NewDevice() {
                                         handleDeviceSelect(item)
                                     }>
                                     <Text
-                                        style={{
-                                            fontFamily: 'Cirka-Bold',
-                                            color: '#eee',
-                                            fontSize: 18,
-                                        }}
-                                    >
-                                        {item.key} : {item.host}
+                                    style={{
+                                        fontFamily: 'Cirka-Bold',
+                                        color: '#eee',
+                                        fontSize: 18,
+                                    }}   
+                                     >
+                                    {item.key} : {item.host}
                                     </Text>
-                                </TouchableOpacity>)}
-                            keyExtractor={(item, index) => `${item.key}-${index}`} />
+                                    </TouchableOpacity>)}
+                                keyExtractor={(item, index) => `${item.key}-${index}`} />
                     </View>
                 )}
 
 
-
+                
             </View>
-            <TouchableOpacity
-                style={styles.cancelButton}
-                onPress={cancelSearch}
-            >
-                <Text style={styles.cancelButtonText}>Cancel</Text>
-            </TouchableOpacity>
-
-        </View>
+                <TouchableOpacity
+                    style={styles.cancelButton}
+                    onPress={cancelSearch}
+                >
+                    <Text style={styles.cancelButtonText}>Cancel</Text>
+                </TouchableOpacity>
+                
+                </View>
     );
 }
 
@@ -150,6 +172,3 @@ const styles = StyleSheet.create({
         fontSize: 18,
     },
 });
-
-
-
